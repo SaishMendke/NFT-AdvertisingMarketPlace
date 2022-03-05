2652c6a8f33ad015bd9dfab237769a57156218da
Saish Mendke <saishmendke10@gmail.com>
2022-03-05 14:31:24 +0530

    Added files

---
+++ pages/dashboard.js
@@ -0,0 +1,89 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+
+import {
+  marketplaceAddress
+} from '../config'
+
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
+import { AbiCoder } from 'ethers/lib/utils'
+
+export default function CreatorDashboard() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal({
+      network: 'mainnet',
+      cacheProvider: true,
+    })
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+    const data = await contract.fetchItemsListed()
+
+    const items = [
+      {company: 'ABC',
+        price: 1,
+        tokenId: 121,
+        seller: 121,
+        owner: 121,
+        tokenURI: 'demo'}, 
+      {company: 'XYZ',
+        price: 1,
+        tokenId: 122,
+        seller: 122,
+        owner: 122,
+        tokenURI: 'demo'},
+      {company: 'PQR',
+        price: 2,
+        tokenId: 123,
+        seller: 123,
+        owner: 123,
+        tokenURI: 'demo'}
+    ]
+    /*await Promise.all(data.map(async i => {
+      const tokenUri = await contract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+      }
+      return item
+    }))*/
+
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>)
+  return (
+    <div>
+      <div className="p-4">
+        <h2 className="text-2xl py-2">Items Listed</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {
+            nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} className="rounded" />
+                <div className="p-4 bg-black">
+                  <p className="text-2xl font-bold text-white">Company - {nft.company}</p>
+                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
